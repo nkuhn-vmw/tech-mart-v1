9e8d893b13a4350afb10226af78fdbf653a64317
vcap <vcap@d58253b6-7e3e-4d1f-564f-e891>
2026-04-23 01:24:48 +0000

    Add comment to ProductController for CRUD endpoints

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// CRUD endpoints for Product
+
 import java.util.Optional;
 
 @RestController
